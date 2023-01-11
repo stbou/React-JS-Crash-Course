6d715c2b2e870f3c231bb4341b2741cc7c242adb
Stephane Boulanger <stephboul22@gmail.com>
2023-01-10 21:10:13 -0500

    first draft

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import { useState } from "react"
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
-const App = () => {  
-
-
+const App = () => { 
 
   const [tasks, setTasks] = useState([
     {
@@ -27,10 +25,15 @@ const App = () => {
     }  
 ]);
 
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks To Show'}
     </div>
   );
 }
