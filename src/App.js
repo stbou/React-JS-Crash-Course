5c61e2053beec17af814225c7abfa548a334bb2b
Stephane Boulanger <stephboul22@gmail.com>
2023-01-10 20:44:34 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useState } from "react"
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 const App = () => {  
+
+
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 3:30pm',
+        reminder: true,
+    }, 
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 7th at 4:30pm',
+        reminder: false,
+    }  
+]);
+
   return (
-    <div className="App">
+    <div className='container'>
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 
 const Header = ({ title }) => {
-  return (
-    <header>
-      <h1>{title}</h1>
-    </header>
-  )
+
+    const onClick = () => {
+        console.log('click')
+    }
+    
+    return (
+        <header className='header'>
+        <h1>{title}</h1>
+        <Button color='green' text='Add' onClick={ onClick }/>
+        </header>
+    )
 }
 
 Header.defaultProps = {
@@ -16,5 +23,11 @@ Header.defaultProps = {
 Header.prototype = {
     title: PropTypes.string.isRequired,
 }
+
+// CSS in JS
+// const headingStyle = {
+//    color:'red', 
+//    backgroundColor:'black'
+// }
 
 export default Header
