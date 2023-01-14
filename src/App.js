b29277dce6812fdafbc54d63fce52fae99610a80
Stephane Boulanger <stephboul22@gmail.com>
2023-01-14 12:38:31 -0500

    json server

--- src/App.js
+++ src/App.js
@@ -7,26 +7,7 @@ const App = () => {
 
   const [showAddTask, setShowAddTask] = useState(false); 
 
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 3:30pm',
-        reminder: true,
-    }, 
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 7th at 4:30pm',
-        reminder: false,
-    }  
-]);
+  const [tasks, setTasks] = useState([]);
 
 // Add Task
 const addTask = (task) => {
